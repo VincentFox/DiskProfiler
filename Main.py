f1b5966b217530ba113e6f066567c6c40b9297e6
Vincent <vincentfox1992@gmail.com>
2018-11-08 09:23:20 +0800

    Update Main.py

---
+++ Main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import fire
+
+class Calculator(object):
+
+  def double(self, number):
+    return 2 * number
+
+if __name__ == '__main__':
+  fire.Fire(Calculator)
+
